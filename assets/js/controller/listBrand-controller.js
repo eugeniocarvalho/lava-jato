7a8d08c6973befe6a846dd28d861e6c5fe2d0ab7
Eugênio Carvalho <eugeniofrcarvalho@gmail.com>
2022-08-02 15:05:00 -0300

    showing data from api

--- assets/js/controller/listBrand-controller.js
+++ assets/js/controller/listBrand-controller.js
@@ -9,20 +9,31 @@ const createOption = (name, id) => {
   return option;
 }
 
-const addOptiontoTable = (name, id) => {
-  const tr = document.createElement("tr");
+const addnewBrandToTable = (name) => {
+  const newTr = document.createElement("tr");
   const content = `
             <td>${name}</td>
             <td class="icone-editar"><i class="fa-solid fa-pencil"></i></td>
             <td class="icone-deletar"><i class="fa-regular fa-trash-can"></i></td>
           `;
+
+  newTr.innerHTML = content;
+
+  return newTr;
 }
 
 const insertSelect = document.querySelector(".form-select");
+const brandTable = document.querySelector("[data-table]");
 
 brandService.listBrands()
   .then(data => {
     data.forEach(brand => {
-      insertSelect.appendChild(createOption(brand.name, brand.id));
+      if (insertSelect != null) {
+        insertSelect.appendChild(createOption(brand.name, brand.id));
+      }
+
+      if (brandTable != null)
+        brandTable.appendChild(addnewBrandToTable(brand.name));
+
     });
   });
