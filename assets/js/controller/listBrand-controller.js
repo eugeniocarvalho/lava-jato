4ae55dda0a8786dbd3e302d04bd2a4fa917734d7
Eugênio Carvalho <eugeniofrcarvalho@gmail.com>
2022-08-02 14:42:41 -0300

    layout to pages

--- assets/js/controller/listBrand-controller.js
+++ assets/js/controller/listBrand-controller.js
@@ -9,6 +9,15 @@ const createOption = (name, id) => {
   return option;
 }
 
+const addOptiontoTable = (name, id) => {
+  const tr = document.createElement("tr");
+  const content = `
+            <td>${name}</td>
+            <td class="icone-editar"><i class="fa-solid fa-pencil"></i></td>
+            <td class="icone-deletar"><i class="fa-regular fa-trash-can"></i></td>
+          `;
+}
+
 const insertSelect = document.querySelector(".form-select");
 
 brandService.listBrands()
